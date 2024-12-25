a01a841d439b54d079b8e16ab6bb83d2e55cfb1a
jiyaa-patel <jiyarajpatel@gmail.com>
2024-12-25 18:58:33 +0530

    task1, task2 completed uncompleted task3

--- ToDoList.py
+++ ToDoList.py
@@ -8,17 +8,27 @@ def add():
     deadline = input("Enter deadline- ")
     with open('ToDoList.txt' , 'r') as file:
         content = file.readlines()
-        content.insert(1, "    " + task + "\n    " + deadline + "\n")
+        content.insert(1, "\n    " + task + "\n    " + deadline + "\n")
         addedtask = ''.join(content)
     with open('ToDoList.txt' , 'w') as file:
         file.write(addedtask)
+    show()
+    
+def delete():
+    deleteTask = int(input("Enter the number of the task to be delted: "))
+    with open('ToDoList.txt', 'r') as file:
+        content = file.readlines()
+        del content[]
         
 print("----------------> TO DO LIST <-------------------")
 print("Select from the below: \n1.Show the list\n2.Add a new task\n3.Delete a task\n")
 task = int(input(" "))
-# print(task)
+
 if task == 1:
     show()
 
 elif task == 2:
     add()
+
+elif task == 3:
+    delete()
